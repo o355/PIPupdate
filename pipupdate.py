56ebbfaaa9dac3dbfd5b33973b54c3bb8ab7bcd1
o355 <o355@users.noreply.github.com>
2016-12-26 23:02:03 -0500

    Update pipupdate.py
    Added a few minor features, will be propagated to a release soon.

--- pipupdate.py
+++ pipupdate.py
@@ -1,19 +1,26 @@
-# PIPupdate
+# PIPupdate 0.9.1
 # (c) 2016 o355 under the GNU GPL 3.0
 
-print("Welcome to PIPupdate (v0.9)")
+print("Welcome to PIPupdate (v0.9.1)")
 print("Loading...")
 #Declaring number vars
 updatecountint = 0
 updatenumber = 0
 #Try/Except for proper handling of possible import errors
 try:
+    import sys
+except ImportError:
+    raise ImportError("Please install sys to use PIPupdate!")
+try:
     import pip
 except ImportError:
     print("Please install PIP to use PIPupdate!")
     sys.exit()
-import sys
-from subprocess import call
+try:
+    from subprocess import call
+except ImportError:
+    print("Please install subprocess/subprocess call to use PIPupdate!")
+    sys.exit()
 #Here we find out how many packages we need to install
 for pkgname in pip.get_installed_distributions():
     updatenumber = updatenumber + 1
