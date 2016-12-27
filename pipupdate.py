c921757f95da11d3906ce877ab4198047ad429ba
o355 <o355@users.noreply.github.com>
2016-12-26 21:43:35 -0500

    Major codebase change
    Added some extra stuff as well

--- pipupdate.py
+++ pipupdate.py
@@ -1,19 +1,29 @@
 #PIPupdate
 #(c) 2016 o355 under the GNU GPL 3.0
 
-import sys
-print("Welcome to PIPupdate (v0.9).")
+print("Welcome to PIPupdate (v0.9)")
 print("Loading...")
+updatecountint = 0
+updatenumber = 0
 try:
     import pip
 except ImportError:
     print("Please install PIP to use PIPupdate!")
     sys.exit()
-
+import sys
+from subprocess import call
 for pkgname in pip.get_installed_distributions():
-    print("Now attempting to update package " + pname.project_name)
-    call("pip install --upgrade " + pname.project_name, shell=True)
-    updates + 1
-    print("Updated package " + pname.project_name + " successfully (finished", updates, "updates so far.)")
+    updatenumber = updatenumber + 1
+updatenumberstr = str(updatenumber)
+for pkgname in pip.get_installed_distributions():
+    print("PIPupdate: Now attempting to update package " + pkgname.project_name)
+    print("")
+    updatecountint = updatecountint + 1
+    updatecountstr = str(updatecountint)
+    call("pip install --upgrade " + pkgname.project_name, shell=True)
+    print("")
+    print("PIPupdate: Updated package " + pkgname.project_name + " successfully (finished " + updatecountstr + "/" + updatenumberstr + " updates so far.)")
 
-print("PIPupdate is done!")
+print("")
+print("PIPupdate is done, upgraded " + updatecountstr + " packages!")
+print("Thank you for using PIPupdate!")
